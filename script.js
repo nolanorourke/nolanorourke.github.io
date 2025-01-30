3f4e12ffd0fed17450b59185c638b4a9bad523ae
Nolan ORourke <ngorourke@gmail.com>
2025-01-30 02:25:03 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function()
+{
+    var linkElements = document.head.querySelectorAll("link");
+
+    linkElements.forEach(function(link){
+        if(choice == "Navy")
+        {
+
+        }
+        else if(choice == "Maroon")
+        {
+            link.href = 
+        }
+        else if(choice == "Forest Green")
+        {
+
+        }
+        else 
+        {
+
+        }
+    });
+});
